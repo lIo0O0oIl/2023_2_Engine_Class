6342500499df46d8d2a4142a2bdf318dcd8d060e
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2023-09-01 09:13:47 +0900

    FOVStencil 기초 설정
    - 인풋 시스템, 맵 생성, 파일 나누기

---
+++ FOVStencil/Assets/01. Scripts/SO/InputReader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Controls;
+
+public class InputReader : ScriptableObject, IPlayerActions
+{
+    public void OnAIM(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnMovement(InputAction.CallbackContext context) 
+    {
+
+    }
+}
